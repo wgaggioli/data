880cfbe57d1744d2cba3701d9c3527fc621f25fd
William Gaggioli <wgaggioli@gmail.com>
2014-09-17 17:26:35 -0500

    change assertEqual to assertIs to prevent Tanner from cheating

--- test_data.py
+++ test_data.py
@@ -6,12 +6,12 @@ from data import data as data_cls, infinity
 class _data(unittest.TestCase):
     def data(self):
         data = data_cls()
-        self.assertEqual(data.data, data)
-        self.assertEqual(eval('data.' * infinity + 'data', {}, {"data": data}),
-                         float('inf') - infinity)
-        self.assertEqual(eval('data.' * (infinity - 1) + 'data', {},
-                              {"data": data}),
-                         data)
+        self.assertIs(data.data, data)
+        self.assertIs(eval('data.' * infinity + 'data', {}, {"data": data}),
+                      float('inf') - infinity)
+        self.assertIs(eval('data.' * (infinity - 1) + 'data', {},
+                           {"data": data}),
+                      data)
 
     def test_data(self):
         self.data()
