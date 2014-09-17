740e467fd154b3b25129c04407fb6fb08ad647bb
William Gaggioli <wgaggioli@gmail.com>
2014-09-17 17:22:54 -0500

    eval with context

--- test_data.py
+++ test_data.py
@@ -7,9 +7,11 @@ class _data(unittest.TestCase):
     def data(self):
         data = data_cls()
         self.assertEqual(data.data, data)
-        self.assertEqual(eval('data.' * infinity + 'data'),
+        self.assertEqual(eval('data.' * infinity + 'data', {}, {"data": data}),
                          float('inf') - infinity)
-        self.assertEqual(eval('data.' * (infinity - 1) + 'data'), data)
+        self.assertEqual(eval('data.' * (infinity - 1) + 'data', {},
+                              {"data": data}),
+                         data)
 
     def test_data(self):
         self.data()
