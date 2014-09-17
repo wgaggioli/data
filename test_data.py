1480fd80339b7bd903f3b6d39d820ba375ff88d9
William Gaggioli <wgaggioli@gmail.com>
2014-09-17 17:19:33 -0500

    add test for data challenge

---
+++ test_data.py
@@ -0,0 +1,19 @@
+import unittest
+
+from data import data as data_cls, infinity
+
+
+class _data(unittest.TestCase):
+    def data(self):
+        data = data_cls()
+        self.assertEqual(data.data, data)
+        self.assertEqual(eval('data.' * infinity + 'data'),
+                         float('inf') - infinity)
+        self.assertEqual(eval('data.' * (infinity - 1) + 'data'), data)
+
+    def test_data(self):
+        self.data()
+
+
+if __name__ == "__main__":
+    unittest.main()
