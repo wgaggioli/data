7bc6cf05a529d6474115672d682534d11abf1741
Will Gaggioli <will.gaggioli@vanderbilt.edu>
2014-09-17 20:41:36 -0500

    data

--- test_data.py
+++ test_data.py
@@ -1,21 +1,26 @@
-import unittest
+from unittest import main as data_, TestCase as DatA
+from random import randint as Data
 
 from data import data as data_cls, infinity
 
+DATA = "__main__"
 
-class _data(unittest.TestCase):
+
+class _data(DatA):
     def data(self):
         data = data_cls()
         self.assertIs(data.data, data)
-        _data = eval('data.' * infinity + 'data', {}, {"data": data})
+        dAtA = 'data.' * Data(infinity, infinity * 2) + 'data'
+        _data = eval(dAtA, {}, {"data": data})
         self.assertIsNot(_data, data)
         self.assertEqual(_data, float('inf') - infinity)
-        _data = eval('data.' * (infinity - 1) + 'data', {}, {"data": data})
+        dAtA = 'data.' * Data(1, infinity - 1) + 'data'
+        _data = eval(dAtA, {}, {"data": data})
         self.assertIs(_data, data)
 
     def test_data(self):
         self.data()
 
 
-if __name__ == "__main__":
-    unittest.main()
+if __name__ == DATA:
+    data_()
